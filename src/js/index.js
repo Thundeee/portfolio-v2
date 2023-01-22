f69bdab10a87b4741fe66733b6eed51af3b57527
Thundeee <asbjornabel@gmail.com>
2023-01-22 15:32:16 +0100

    added tech, more about me and projects

---
+++ src/js/index.js
@@ -0,0 +1,58 @@
+import { projects } from "./utils/projects.js";
+
+if (document.querySelector('.historyReturn')) {
+    document.querySelector('.historyReturn').addEventListener('click', () => {
+console.log('historyReturn');    });
+}
+    
+if (window.location.pathname === '/index.html' || window.location.pathname === '/') {
+    window.onbeforeunload = function() {
+    sessionStorage.setItem('scrollIndex', window.scrollY);
+  }
+  
+  window.onload = function() {
+    if (sessionStorage.getItem('scrollIndex')) {
+      window.scrollTo(0, sessionStorage.getItem('scrollIndex'));
+    }
+  }
+
+}
+
+let dropdown = document.querySelector('.dropdown');
+let dropdownText = document.querySelector('.dropdown-toggle');
+let currentText = dropdownText.innerText;
+let content = document.querySelector('.content');
+
+dropdown.addEventListener('click', function(event) {
+
+        if (event.target.innerText === currentText) {
+            return;
+        }
+
+
+        dropdownText.innerText = event.target.innerText;
+        currentText = dropdownText.innerText;
+
+        // IMPORT DUMPS FRA ANNEN FIL OG PUT INNERHTML SOM IMPORTEN
+        console.log(currentText)
+        switch (currentText) {
+            case "Auction House":
+              console.log("Auction House");
+              console.log(projects[0])
+              content.innerHTML = projects[0];
+              break;
+            case "Unite Bot":
+              console.log("Unite Bot");
+              console.log(projects[1])
+              content.innerHTML = projects[1];
+
+              break;
+              case "Workflow":
+                console.log("Workflow");
+                console.log(projects[2])
+                content.innerHTML = projects[2];
+
+                break;
+          }
+});
+  
