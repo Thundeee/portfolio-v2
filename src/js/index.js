09f2162712abc2210016f3e44f6f6eecfb6b8df6
Asbjørn <78221203+Thundeee@users.noreply.github.com>
2023-01-23 21:53:39 +0100

    added carosell, fixed js bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,58 @@
 import { projects } from "./utils/projects.js";
-if (document.querySelector('.historyReturn')) {
-    document.querySelector('.historyReturn').addEventListener('click', () => {
-console.log('historyReturn');    });
+if (document.querySelector(".historyReturn")) {
+  document.querySelector(".historyReturn").addEventListener("click", () => {});
 }
-    
-if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/portfolio-v2/index.html' || window.location.pathname === '/portfolio-v2/') {
-    window.onbeforeunload = function() {
-    sessionStorage.setItem('scrollIndex', window.scrollY);
-  }
-  
-  window.onload = function() {
-    if (sessionStorage.getItem('scrollIndex')) {
-      window.scrollTo(0, sessionStorage.getItem('scrollIndex'));
+
+if (
+  window.location.pathname === "/index.html" ||
+  window.location.pathname === "/" ||
+  window.location.pathname === "/portfolio-v2/index.html" ||
+  window.location.pathname === "/portfolio-v2/"
+) {
+  window.onbeforeunload = function () {
+    sessionStorage.setItem("scrollIndex", window.scrollY);
+  };
+
+  window.onload = function () {
+    if (sessionStorage.getItem("scrollIndex")) {
+      window.scrollTo(0, sessionStorage.getItem("scrollIndex"));
     }
-  }
-
+  };
 }
 
-if (document.querySelector('.dropdown')) {
-    dropdownFunction();
+if (document.querySelector(".dropdown")) {
+  dropdownFunction();
 }
-
 
 function dropdownFunction() {
-    
+  let dropdown = document.querySelector(".dropdown");
+  let dropdownText = document.querySelector(".dropdown-toggle");
+  let currentText = dropdownText.innerText;
+  let content = document.querySelector(".content");
 
-let dropdown = document.querySelector('.dropdown');
-let dropdownText = document.querySelector('.dropdown-toggle');
-let currentText = dropdownText.innerText;
-let content = document.querySelector('.content');
+  dropdown.addEventListener("click", function (event) {
+    if (
+      event.target.innerText === currentText ||
+      event.target.innerText === undefined ||
+      event.target.innerText.match(/\n/g)
+    ) {
+      return;
+    }
 
-dropdown.addEventListener('click', function(event) {
-console.log(event)
-        if (event.target.innerText === currentText) {
-            return;
-        }
+    dropdownText.innerText = event.target.innerText;
+    currentText = dropdownText.innerText;
 
-
-        dropdownText.innerText = event.target.innerText;
-        currentText = dropdownText.innerText;
-
-        // IMPORT DUMPS FRA ANNEN FIL OG PUT INNERHTML SOM IMPORTEN
-        console.log(currentText)
-        switch (currentText) {
-            case "Auction House":
-              console.log("Auction House");
-              console.log(projects[0])
-              content.innerHTML = projects[0];
-              break;
-            case "Unite Bot":
-              console.log("Unite Bot");
-              console.log(projects[1])
-              content.innerHTML = projects[1];
-
-              break;
-              case "Workflow":
-                console.log("Workflow");
-                console.log(projects[2])
-                content.innerHTML = projects[2];
-
-                break;
-          }
-});
-  }
+    // IMPORT DUMPS FRA ANNEN FIL OG PUT INNERHTML SOM IMPORTEN
+    switch (currentText) {
+      case "Auction House":
+        content.innerHTML = projects[0];
+        break;
+      case "Unite Bot":
+        content.innerHTML = projects[1];
+        break;
+      case "Workflow":
+        content.innerHTML = projects[2];
+        break;
+    }
+  });
+}
